5450315a4e5662f8ddc80b4ef6025943f782d3e4
Himashree Hazarika <52618743+HIMA10SHREE@users.noreply.github.com>
2023-10-09 04:55:09 +0530

    Update About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Home from '../pages/About';
 
 test('renders home component', () => {
-  render(<Home />);
+  render(<About />);
   const headingElement = screen.getByRole('heading', { name: /home/i });
   expect(headingElement).toBeInTheDocument();
 });
