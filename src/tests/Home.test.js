cefb3850888ae77d504e6dcf8fd536ddae1afcdb
Himashree Hazarika <52618743+HIMA10SHREE@users.noreply.github.com>
2023-10-09 04:54:52 +0530

    Update Home.test.js

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import HomePage from '../pages/Home.js';
 
 test('renders homepage component', () => {
-  render(<Home/>);
+  render(<Home />);
   const headingElement = screen.getByRole('heading', { name: /welcome to my website/i });
   expect(headingElement).toBeInTheDocument();
 });
