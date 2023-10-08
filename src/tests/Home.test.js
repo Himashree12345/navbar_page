ebb9c66668067fd40c1582192f9b3d0fde9e0236
Himashree Hazarika <52618743+HIMA10SHREE@users.noreply.github.com>
2023-10-09 04:45:45 +0530

    Update Home.test.js

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import HomePage from '../pages/HomePage';
+import HomePage from '../pages/Home.js';
 
 test('renders homepage component', () => {
   render(<HomePage />);
