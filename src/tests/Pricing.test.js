dede528a1e6cf18e74e37a05faa1f2dbef927a09
Himashree12345 <141227797+Himashree12345@users.noreply.github.com>
2023-10-09 10:51:21 +0530

    Update Pricing.test.js

--- src/tests/Pricing.test.js
+++ src/tests/Pricing.test.js
@@ -3,6 +3,6 @@ import Pricing from '../pages/Pricing';
 
 test('renders pricing component', () => {
   render(<Pricing />);
-  const headingElement = screen.getByRole('heading', { name: /pricing/i });
+  const headingElement = screen.getByRole('heading', { name: /Pricing/i });
   expect(headingElement).toBeInTheDocument();
 });
