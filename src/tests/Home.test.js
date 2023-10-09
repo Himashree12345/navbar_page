33366df12c23433633b715a86d64862dc840f56e
Himashree12345 <141227797+Himashree12345@users.noreply.github.com>
2023-10-09 10:50:46 +0530

    Delete src/tests/Home.test.js

--- src/tests/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from '../pages/Home';
-
-test('renders homepage component', () => {
-  render(<Home />);
-  const headingElement = screen.getByRole('heading', { name: /welcome to my website/i });
-  expect(headingElement).toBeInTheDocument();
-});
